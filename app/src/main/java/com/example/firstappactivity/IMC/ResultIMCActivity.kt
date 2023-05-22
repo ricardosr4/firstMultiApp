7d23ca956eca6270a7a9cd09917887ef22efd233
ricardosr4 <ricardosr4@gmail.com>
2023-05-22 10:53:01 -0400

    segunda app lista

---
+++ app/src/main/java/com/example/firstappactivity/IMC/ResultIMCActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstappactivity.IMC
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstappactivity.R
+
+class ResultIMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_imcactivity)
+    }
+}
